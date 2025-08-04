9d5d28a4d2577dbb5677890277f4b53a7d90f823
Thamalu7501 <thamalu7501@gmail.com>
2025-08-04 18:12:36 +0530

    Created featured post and related posts for Projects

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,18 @@
 import { Toaster } from 'react-hot-toast';
 import './App.css'
-
+import Home from './pages/Home/home'
+import Admin from './pages/Admin/admin'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
 
   return (
+    
     <BrowserRouter>
     <Toaster position="top-right" reverseOrder={false}/>
       <Routes path="/*">
 
-        <Route path="/admin/*" element={<AdminPage />} /> 
+        <Route path="/admin/*" element={<Admin />} /> 
+        <Route path="/*" element={<Home />} />
   
       </Routes>  
     </BrowserRouter>
